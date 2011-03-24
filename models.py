1c88bbfa731044c6e6fcb2da86db67f85665e6b6
Sigurd Gartmann <sigurdga@sigurdga.no>
2011-03-24 19:13:29 +0100

    lessen requirements on url field so we can use it

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ from mptt.models import MPTTModel
 class Menu(MPTTModel):
     """MPTT tree menu"""
     name = models.CharField(max_length=20)
-    url = models.URLField(blank=True, verify_exists=True)
+    url = models.CharField(max_length=100, db_index=True)
     user = models.ForeignKey(User)
     updated_date = models.DateTimeField(auto_now=True)
     parent = models.ForeignKey('self', null=True, blank=True, related_name='children')
@@ -13,7 +13,7 @@ class Menu(MPTTModel):
     class Meta:
         ordering = ["name"]
         verbose_name, verbose_name_plural = "menu", "menus"
-        
+
     class MPTTMeta:
         pass
         #order_insertion_by=['name']
