c92c0d6fe626eba182b10ba58d959315ec45d12d
Sigurd Gartmann <sigurdga@sigurdga.no>
2011-06-21 13:39:53 +0200

    Get templates with the package

--- setup.py
+++ setup.py
@@ -5,4 +5,5 @@ setup(
         name = 's7n-menu',
         version = "1a1",
         packages = ['s7n.menu', 's7n.menu.templatetags'],
+        package_data = {'s7n.menu': ['templates/menu/*.html']},
         )
