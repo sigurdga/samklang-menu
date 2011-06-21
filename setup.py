285faec69b453e256b711bcc635374b6b4ddcc8d
Sigurd Gartmann <sigurdga@sigurdga.no>
2011-06-21 11:53:55 +0200

    Add templatetags package.

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ from distutils.core import setup
 setup(
         name = 's7n-menu',
         version = "1a1",
-        packages = ['s7n', 's7n.menu'],
+        packages = ['s7n.menu', 's7n.menu.templatetags'],
         )
