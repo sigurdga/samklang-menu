53f318c7a77f1841ade1eaf602ef002ab80c4367
Sigurd Gartmann <sigurdga@sigurdga.no>
2011-06-23 19:42:34 +0200

    Cleanup whitespace

--- s7n/menu/views.py
+++ s7n/menu/views.py
@@ -15,12 +15,12 @@ def list_menu(request, tree_id):
     root_node = Menu.tree.root_node(tree_id)
     if not root_node:
         raise Http404
-    
+
     if request.method == 'POST':
         try:
             # get tree from json-data
             tree = simplejson.loads(request.POST["mptt_tree"])
-            
+
             # format tree, remove root-item and make root-parents = id
             root = None
             for node in tree:
@@ -28,20 +28,20 @@ def list_menu(request, tree_id):
                     node['parent_id'] = root_node.id
                 if node['item_id'] == 'root':
                     root = node
-            
+
             if root:
                 tree.remove(root)
-            
+
             cursor = connection.cursor()
             qn = connection.ops.quote_name
             opts = root_node._meta
-        
+
             cursor.executemany("""
-                UPDATE %(table)s 
+                UPDATE %(table)s
                 SET %(parent)s = :parent_id,
                     %(left)s = :left,
                     %(right)s = :right,
-                    %(level)s = :depth 
+                    %(level)s = :depth
                 WHERE %(pk)s = :item_id AND %(tree_id)s = %(given_tree_id)s""" % {
                     "table": qn(opts.db_table),
                     'level': qn(opts.get_field(opts.level_attr).column),
@@ -53,12 +53,12 @@ def list_menu(request, tree_id):
                     'given_tree_id': tree_id,
                 }, tree)
             transaction.commit_unless_managed()
-            
+
         except (KeyError, ValueError):
             pass
-        
+
         return HttpResponseRedirect(reverse("strekmann_menu_list", args=[tree_id]))
-        
+
     menu = root_node.get_descendants().all()
     menus = Menu.tree.root_nodes().all()
     
@@ -68,11 +68,11 @@ def list_menu(request, tree_id):
             'menu': menu,
             'menus': menus,
             'tree_id': tree_id,
-        }, 
+        },
         context_instance=RequestContext(request))
-      
 
-  
+
+
 def new_menuitem(request, tree_id):
     """Create a new menu item - return html for ajax injection"""
     if request.method == 'POST':
@@ -88,17 +88,17 @@ def new_menuitem(request, tree_id):
         if request.method == 'GET' and 'parent_id' in request.GET:
             menu.parent_id = request.GET['parent_id']
         form = MenuForm(instance=menu)
-    
+
     return render_to_response(
-        'strekmann_menu/new_item.html', 
-        {'form': form, 'tree_id': tree_id}, 
+        'strekmann_menu/new_item.html',
+        {'form': form, 'tree_id': tree_id},
         context_instance=RequestContext(request))
-        
+
 def delete_menuitem(request, tree_id):
     """Delete a menu item"""
     if request.method == 'POST' and 'node_id' in request.POST:
         node = Menu.objects.get(pk=request.POST['node_id'], tree_id=tree_id)
         node.delete()
         return HttpResponse("1")
-        
+
     return HttpResponse("0")
