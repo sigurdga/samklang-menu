6db8ce8895f228fb6b21c3f749b2503f6546715f
Sigurd Gartmann <sigurdga@sigurdga.no>
2011-07-03 21:35:40 +0200

    add translations and migrations

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ from distutils.core import setup
 setup(
         name = 's7n-menu',
         version = "1a1",
-        packages = ['s7n.menu', 's7n.menu.templatetags'],
-        package_data = {'s7n.menu': ['templates/menu/*.html']},
+        packages = ['s7n.menu', 's7n.menu.templatetags', 's7n.menu.migrations'],
+        package_data = {'s7n.menu': ['templates/menu/*.html', 'locale/*/LC_MESSAGES/django.*o']},
         )
