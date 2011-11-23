c30b876090fbedd49bc616518efba5b12eb69da7
Sigurd Gartmann <sigurdga@sigurdga.no>
2011-11-23 23:42:07 +0100

    Make code clearer, use some tests.

--- samklang_menu/templatetags/menu_tags.py
+++ samklang_menu/templatetags/menu_tags.py
@@ -1,5 +1,6 @@
 from samklang_menu.models import Menu
 from django import template
+from django.core.exceptions import ObjectDoesNotExist
 
 register = template.Library()
 
@@ -10,31 +11,62 @@ def make_menu(parent, user):
             menu.append(child)
     return menu
 
-def html_menu(menu):
-    return "<ul>" + "".join([ '<li><a href="%s">%s</a></li>' % (item.url, item.name) for item in menu ]) + "</ul>"
+def html_menu(menu, active_link=None):
+    menu_contents = []
+    for item in menu:
+        if item==active_link:
+            class_expr = 'class="active"'
+        else:
+            class_expr = ''
+        menu_contents.append('<li><a href="%s" %s>%s</a></li>' % (item.url, class_expr, item.name))
+    return "<ul>" + "".join(menu_contents) + "</ul>"
+
+def find_active(site_id, path):
+    """
+    Find active link by stripping off one part of the url at a time.
+    """
+    active_link = None
+    while not active_link and active_link != "/":
+        try:
+            active_link = Menu.objects.get(tree_id=site_id, url=path)
+            break
+        except ObjectDoesNotExist:
+            if path.endswith("/"):
+                path = path.rsplit("/", 2)[0] + "/"
+            else:
+                path = path.rsplit("/", 1)[0] + "/"
+    return active_link
+
+def submenu(active_link, user):
+    """
+    Will actually get submenu of the first parent it can show
+    """
+    menu = make_menu(active_link, user)
+    while not menu and active_link.parent:
+        active_link = active_link.parent
+        menu = make_menu(active_link, user)
+    return menu
 
 @register.simple_tag
-def menu(request, default_url="/"):
+def simple_menu(request, default_url="/"):
+
     if hasattr(request, 'site'):
         site_id = request.site.id
     else:
         site_id = 1
-    user = request.user
-    path = request.path_info
-    try:
-        active_link = Menu.objects.get(tree_id=site_id, url=path)
-        menu = make_menu(active_link, user)
-        while not menu and active_link.parent: #empty?
-            active_link = active_link.parent
-            menu = make_menu(active_link, user)
-    except Menu.DoesNotExist:
-        active_link = Menu.objects.get(tree_id=site_id, url=default_url)
-        menu = make_menu(active_link, user)
-        while not menu and active_link.parent:
-            active_link = active_link.parent
-            menu = make_menu(active_link, user)
 
-    return html_menu(menu)
+    # find the closest parent to the active url
+    active_link = find_active(site_id, request.path_info)
+
+    # find the menu closest to the active_link
+    menu = submenu(active_link, request.user)
+
+    return html_menu(menu, active_link)
+
+@register.simple_tag
+def menu(request, default_url="/"):
+    return simple_menu(request, default_url)
+
 
 @register.simple_tag
 def breadcrumbs(request, extra=[]):
--- samklang_menu/tests.py
+++ samklang_menu/tests.py
@@ -7,17 +7,48 @@ Replace these with more appropriate tests for your application.
 
 from django.test import TestCase
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
+from django.contrib.auth.models import User
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
+from samklang_menu.templatetags.menu_tags import find_active, submenu, html_menu
+from samklang_menu.models import Menu
 
->>> 1 + 1 == 2
-True
-"""}
+class FindActiveTest(TestCase):
+
+    def test_find_main(self):
+        """
+        Find main page even if main page is not in menu. Also checks empty
+        search.
+        """
+        m = Menu(name="parent", url="/sitemap/", tree_id=1, user_id=1)
+        m.save()
+        m2 = Menu(name="main", url="/", tree_id=1, parent=m, user_id=1)
+        m2.save()
+        self.assertEqual(m2, find_active(1, "/"))
+        self.assertEqual(m2, find_active(1, ""))
+
+    def test_find_forum_new(self):
+        """
+        /forum/new/ does not exist, but /forum/ does. Checks for /forum/new
+        without trailing slash as well.
+        """
+        m = Menu(name="parent", url="/sitemap/", tree_id=1, user_id=1)
+        m.save()
+        m2 = Menu(name="main", url="/", tree_id=1, parent=m, user_id=1)
+        m2.save()
+        m3 = Menu(name="forum", url="/forum/", tree_id=1, parent=m, user_id=1)
+        m3.save()
+        self.assertEqual(m3, find_active(1, "/forum/new/"))
+        self.assertEqual(m3, find_active(1, "/forum/new"))
+
+class MakeMenuTest(TestCase):
+
+    def test_submenu(self):
+        u = User()
+        m = Menu(name="parent", url="/sitemap/", tree_id=1, user_id=1)
+        m.save()
+        m2 = Menu(name="main", url="/", tree_id=1, parent=m, user_id=1)
+        m2.save()
+        m3 = Menu(name="forum", url="/forum/", tree_id=1, parent=m, user_id=1)
+        m3.save()
+        self.assertEqual([m2, m3], submenu(m3, u))
 
