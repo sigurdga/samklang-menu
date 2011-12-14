7b5c08cad4b8a1a3f9c7a35bade447051edb8ed4
Sigurd Gartmann <sigurdga@sigurdga.no>
2011-12-14 01:15:06 +0100

    Widgets are part of menu app now.
    Widgets can be attached to anything that is in the menu.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
         name='samklang-menu',
-        version="0.3.0",
+        version="0.3.1",
         author='Sigurd Gartmann',
         author_email='sigurdga-samklang@sigurdga.no',
         url='http://github.com/sigurdga/samklang-menu',
@@ -12,6 +12,7 @@ setup(
         license="AGPL",
         packages = ['samklang_menu', 'samklang_menu.templatetags', 'samklang_menu.migrations'],
         package_data = {'samklang_menu': ['templates/samklang_menu/*.html', 'static/img/*.png', 'static/js/*.js', 'locale/*/LC_MESSAGES/django.*o']},
+        py_modules=['samklang_menu.widgets'],
         install_requires=['django-mptt>=0.5.1'],
         classifiers=[
                 "Development Status :: 3 - Alpha",
